de8ad6b2a63c3b212ae148aff950dff8103dedda
Prakash Poudel <prakaas,poudell@gmail.com>
2024-01-03 14:42:31 +0545

    no test

--- src/utils/firebaseData.test.ts
+++
@@ -1,7 +0,0 @@
-import {expect, test} from 'vitest'
-import { firebaseData } from './firebaseData'
-
-test('test type of fetching single firebase document', () => {
-    const {getOneCard} = firebaseData()
-    expect(getOneCard('test@gmail.com')).toBeTypeOf('object')
-})
